482438c9d20621afeb0176fe405f15a961c97122
kiralightyagami <crucisgamer98@gmail.com>
2025-07-23 17:43:50 +0530

    created upload route

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import { Geist_Mono } from "next/font/google";
+import PuterInit from "../components/PuterInit";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -17,19 +18,17 @@ export const metadata: Metadata = {
     description: "Log in to your account",
   };
   
-  export default function RootLayout({
+  export default function Layout({
     children,
   }: Readonly<{
     children: React.ReactNode;
   }>) {
     return (
-      <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-          {children}
-        </body>
-      </html>
+      <div>
+        <PuterInit />
+        <script src="https://js.puter.com/v2/"></script>
+        {children}
+      </div>
     );
   }
   
---
+++ app/upload/page.tsx
@@ -0,0 +1,78 @@
+"use client"
+import { Navbar } from "../components/navbar";
+import { useState } from "react";
+import FileUploader from "../components/FileUploader";
+
+
+
+
+export default function Upload() {
+const [isProcessing, setIsProcessing] = useState(false);
+const [statusText, setStatusText] = useState("");
+const [file, setFile] = useState<File | null>(null);
+
+const handleFileSelect = (file: File | null) => {
+    setFile(file);
+}
+
+const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const form = e.currentTarget.closest("form");
+    if(!form) return;
+    const formData = new FormData(form);
+    const companyName = formData.get("company-name");
+    const jobTitle = formData.get("job-title");
+    const jobDescription = formData.get("job-description");
+    const resumeFile = formData.get("uploader");
+
+    if(!companyName || !jobTitle || !jobDescription || !resumeFile) {
+        setStatusText("Please fill in all fields");
+        return;
+    }
+
+
+}
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+        <Navbar/>
+        <section className="main-section">
+            <div className="page-heading py-16">
+                <h1>Smart feedback for your resume</h1>
+                {
+                    isProcessing ? (
+                        <>
+                          <h2>{statusText}</h2>
+                          <img src="/images/resume-scan.gif" className="w-full"/>
+                        </>
+                    ) : (
+                        <h2>Upload your resume for an ATS and improvement tips</h2>
+                    )}
+                    { !isProcessing && 
+                        <form id="upload-form" onSubmit={handleSubmit} className="flex flex-col gap-4 mt-8">
+                            <div className="form-div">
+                                <label htmlFor="company-name">Company Name</label>
+                                <input type="text" name="company-name" placeholder="Company Name" id="company-name" />
+                            </div>
+                            <div className="form-div">
+                                <label htmlFor="job-title">Job Title</label>
+                                <input type="text" name="job-title" placeholder="Job Title" id="job-title" />
+                            </div>
+                            <div className="form-div">
+                                <label htmlFor="job-description">Job Description</label>
+                                <textarea rows={5} name="job-description" placeholder="Job Description" id="job-description" />
+                            </div>
+                            <div className="form-div">
+                                <label htmlFor="uploader">Upload Resume</label>
+                                <FileUploader onFileSelect={handleFileSelect} />
+                            </div>
+                            <button type="submit" className="primary-button">Analyze Resume</button>
+                        </form>                    
+                    }
+            </div>
+
+        </section>
+    </main>
+  )
+}
+
+
